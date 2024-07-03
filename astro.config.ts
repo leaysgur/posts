d9acb21674987c48c0d80244e8fb720e2480c95c
Yuji Sugiura <y.sugiura.0316@gmail.com>
2024-07-03 11:42:42 +0900

    Use trailingSlash: always instead of build.format: file

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   site: "https://leaysgur.github.io",
   base: PUBLIC_BASE_PATH ?? "",
   // GitHub Pages requires trailing slash w/ `format: directory`
-  build: { format: "file" },
+  trailingSlash: "always",
   // @ts-ignore: type string is too loose
   markdown: { shikiConfig: { theme: PUBLIC_SHIKI_THEME ?? "" } },
 });
